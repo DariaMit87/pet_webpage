5f0103789e78d8915f53d88d6b11b2746cb6a20d
Daria <daria.mitusova87@gmail.com>
2024-02-02 18:57:11 +0100

    works

--- final.js
+++ final.js
@@ -1,16 +1,27 @@
-let currentIndex = 1;
-const totalSlides = $('.img_container').length;
+let currentIndex = 0;
 
 function showSlide(index) {
-    currentIndex = (index + totalSlides) % totalSlides;
-    $('.img_carousel_inner').css('transition', 'transform 0.5s ease-in-out');
-    $('.img_carousel_inner').css('transform', `translateX(${-currentIndex * 410}px)`);
+    const slideWidth = $('.img_container').width();
+    $('.img_carousel_inner').css('transform', `translateX(${-index * slideWidth}px)`);
 }
 
 function nextSlide() {
-    showSlide(currentIndex + 1);
+    const totalSlides = $('.img_container').length;
+    currentIndex = (currentIndex + 1) % totalSlides;
+    showSlide(currentIndex);
 }
 
 function prevSlide() {
-    showSlide(currentIndex - 1);
+    const totalSlides = $('.img_container').length;
+    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+    showSlide(currentIndex);
 }
+
+
+
+let slideshow = document.getElementById('dogpic');
+let images = ["pics/22.webp", "pics/33.webp", "pics/44.webp", "pics/55.webp"]
+setInterval(function(){
+    let rnd = Math.floor(Math.random()*images.length);
+    slideshow.src = images[rnd];
+},1200);
